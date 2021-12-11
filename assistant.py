fd598d541ba7742dc8f98a256546f0b2d4031f4f
sayeedafridi07 <76632275+sayeedafridi07@users.noreply.github.com>
2021-12-11 12:51:01 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -1,21 +1,22 @@
-import pyttsx3 as ts
-import speech_recognition as ani
+import speech_recognition as sr
+import pyttsx3
 import datetime
+import time
 from time import ctime
-import webbrowser
+import pywhatkit
+import pyjokes
 import os
 import subprocess
-import pyjokes
-from youtube import *
+import webbrowser
 from classes import *
 
-engine = ts.init()
-rate = engine.getProperty('rate')
-engine.setProperty('rate', 180)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine = pyttsx3.init()
+rate = engine.getProperty("rate")
+engine.setProperty("rate", 180)
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
 
-gs = ani.Recognizer()
+listener = sr.Recognizer()
 
 
 def speak(audio):
@@ -24,22 +25,22 @@ def speak(audio):
 
 
 def record():
-    with ani.Microphone() as inp:
-        gs.energy_threshold = 10000
-        gs.adjust_for_ambient_noise(inp, 1.2)
+    with sr.Microphone() as inp:
+        listener.energy_threshold = 10000
+        listener.adjust_for_ambient_noise(inp, 1.2)
         print("Listening...")
-        out = gs.listen(inp)
-        store = ''
+        out = listener.listen(inp)
+        command = ""
         try:
-            print('Recognizing...')
-            store = gs.recognize_google(out)
-            print(store)
-        except ani.UnknownValueError:
-            print('sorry didnt get that')
-        except ani.RequestError:
-            print('service error')
+            print("Recognizing...")
+            command = listener.recognize_google(out)
+            print("You: " + command)
+        except sr.UnknownValueError:
+            print("sorry didnt get that")
+        except sr.RequestError:
+            print("service error")
 
-    return store
+    return command.lower()
 
 
 def greet():
@@ -48,126 +49,168 @@ def greet():
         speak("good morning")
     elif hour >= 12 and hour < 16:
         speak("good afternoon")
-    else:
+    elif hour >= 16 and hour < 0:
         speak("good evening")
-    speak("I am your voice assistant")
-    print('What should i call you ?')
-    speak('what should i call you ?')
-    name = record().lower()
-    print('Hello '+name)
-    speak('hello  ' + name)
-    print("How can i help you?")
-    speak("How can i help you?")
+    print("What should i call you ?")
+    speak("what should i call you ?")
+    name = record()
+    print("Hello" + name)
+    speak("Hello " + name)
+    print("How can I help ?")
+    speak("How can I help ?")
 
 
-def respond(store):
+def respond(command):
 
-    if 'what is your name' in store:
-        speak('Well I do not have any name my creaters forget to give me name in hurry')
-    elif "how are you" in store:
-        speak("I am having a good day sir")
-        speak("what can I do for you sir??")
+    # Conversation
 
-    elif 'time' in store:
+    if "hi" and "hello" in command:
+        speak("Hi, it's really good to hear from you! I hope you and your loved ones are safe and healthy")
+
+    elif "time" in command:
         print(ctime())
         speak(ctime())
 
-    elif 'hey good morning' in store:
-        print('Good morning sir.')
-        speak('Good morning sir.')
+    elif "name" in command:
+        speak("Well I do not have any name my creaters forget to give me name in hurry")
 
-    elif 'good evening' in store:
-        print('Good Evening sir.')
-        speak('Good Evening sir.')
+    elif "who are you" and "what you can do" in command:
+        speak("I am your assistant. I can look up answers for you, or help you to play videos. If you need anything just ask, your wish is my command")
 
-    elif 'good night' in store:
-        print('Good Night Sweet Dreams...')
-        speak('Good Night Sweet Dreams...')
+    elif "old" and "age" in command:
+        speak("Well, I'm still pretty new.")
 
-    elif 'open lpu live' in store:
-        print('you are redirecting to LPU LIVE')
-        speak('you are redirecting to LPU LIVE')
-        webbrowser.open("lpulive.lpu.in")
+    elif "how are you" in command:
+        speak("I'm fine. You're very kind to ask especially in these tempestuous times")
 
-    elif 'open o a s' in store:
-        print('you are redirecting to OAS')
-        speak('you are redirecting to OAS')
-        webbrowser.open("oas.lpu.in/")
+    elif "are you single" in command:
+        speak("I am in a relationship with your wifi")
 
-    elif 'open u m s' in store:
-        print('you are redirecting to UMS')
-        speak('you are redirecting to UMS')
-        webbrowser.open("ums.lpu.in/lpuums/")
+    # Activity
 
-    elif 'open my class' in store:
-        print('you are redirecting to My Class')
-        speak('you are redirecting to My Class')
-        webbrowser.open("myclass.lpu.in/")
+    elif "search" in command:
+        print("To find info about somthing, just ask")
+        speak("To find info about somthing, just ask")
+        query = record()
+        url = "https://www.google.co.in/search?q=" + query
+        webbrowser.open(url)
+        speak("Here what I found" + query)
 
-    elif 'schedule' in store:
-        print('you are redirecting to My Class')
-        speak('you are redirecting to My Class')
-        check = myclass()
-        check.schedule()
+    elif "tell me" in command:
+        query = command.replace("tell me", "")
+        url = "https://www.google.co.in/search?q=" + query
+        webbrowser.open(url)
+        speak("Here what I found" + query)
 
-    elif 'open youtube' in store:
-        print('you are redirecting to youtube')
-        speak('you are redirecting to youtube')
-        webbrowser.open("youtube.com")
+    elif "find location" in command:
+        print("Sure, tell me the name of location.")
+        speak("Sure, tell me the name of location.")
+        query = record()
+        url = "https://google.nl/maps/place/" + query
+        webbrowser.open(url)
+        speak("Here what I found" + query)
 
-    elif 'play video on youtube' in store:
-        print('Name of the video you want to play')
-        speak('name of the video you want to play')
-        query = record().lower()
-        print("playing "+query+" on youtube")
-        speak("playing" + query+" on youtube")
-        assist = music()
-        assist.play(query)
+    elif "open youtube" in command:
+        print("Opening Youtube")
+        speak("Opening Youtube")
+        url = "https://www.youtube.com"
+        webbrowser.open(url)
 
-    elif 'search' in store:
-        print('What you want to search')
-        speak('what you want to search')
-        query = record().lower()
-        webbrowser.open("https://www.google.co.in/search?q=" + query)
+    elif "open lpu live" in command:
+        print("you are redirecting to LPU LIVE")
+        speak("you are redirecting to LPU LIVE")
+        url = "https://lpulive.lpu.in/login"
+        webbrowser.open(url)
 
-    elif 'play music' and 'play song' in store:
-        print('Name the platform where you want to listen')
-        speak('name the platform where you want to listen')
+    elif "open o a s" in command:
+        print("you are redirecting to OAS")
+        speak("you are redirecting to OAS")
+        url = "https://oas.lpu.in/"
+        webbrowser.open(url)
 
-        say = record().lower()
-        if 'spotify' in say:
-            print('Name of the song you want to play')
-            speak('name of the song you want to play')
-            query = record().lower()
+    elif "open u m s" in command:
+        print("you are redirecting to UMS")
+        speak("you are redirecting to UMS")
+        url = "https://ums.lpu.in/lpuums/LoginNew.aspx"
+        webbrowser.open(url)
+
+    elif "open my class" in command:
+        print("you are redirecting to My Class")
+        speak("you are redirecting to My Class")
+        url = "https://myclass.lpu.in/"
+        webbrowser.open(url)
+
+    elif "open calculator" in command:
+        print("Opening Calculator")
+        speak("Opening Calculator")
+        subprocess.Popen('C:\\Windows\\System32\\calc.exe')
+
+    elif "open chrome" in command:
+        print("Opening Chrome")
+        speak("Opening Chrome")
+        subprocess.Popen(
+            'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe')
+
+    elif "joke" in command:
+        speak(pyjokes.get_joke())
+
+    # Play
+
+    elif "play video on youtube" in command:
+        print("Name of the video you want to play")
+        speak("Name of the video you want to play")
+        query = record()
+        print("Playing "+query+" on youtube")
+        speak("Playing" + query+" on youtube")
+        pywhatkit.playonyt(query)
+
+    elif "play" in command:
+        query = command.replace("play", "")
+        print("Playing "+query+" on youtube")
+        speak("Playing " + query+" on youtube")
+        pywhatkit.playonyt(query)
+
+    elif "play songs" and "play music" in command:
+        print("Name the platform where you want to play")
+        speak("Name the platform where you want to play")
+
+        say = record()
+        if "spotify" in say:
+            print("Name of the song you want to play")
+            speak("name of the song you want to play")
+            query = record()
             webbrowser.open("https://open.spotify.com/search/" + query)
 
-        elif 'jio saavan' in say:
-            print('Name of the song you want to play')
-            speak('name of the song you want to play')
-            query = record().lower()
+        elif "jio saavan" in say:
+            print("Name of the song you want to play")
+            speak("name of the song you want to play")
+            query = record()
             webbrowser.open("https://www.jiosaavn.com/search/" + query)
 
-        elif 'gaana' in say:
-            print('Name of the song you want to play')
-            speak('name of the song you want to play')
-            query = record().lower()
+        elif "gaana" in say:
+            print("Name of the song you want to play")
+            speak("name of the song you want to play")
+            query = record()
             webbrowser.open("https://gaana.com/search/" + query)
 
-        elif 'joke' in store:
-            joke = pyjokes.get_joke(language='en', category='all')
-            print(joke)
-            speak(joke)
+        # Automation
 
-        elif 'open chrome' in store:
-            print('opening chrome...')
-            speak('opening chrome')
-            dir = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-            os.startfile(dir)
-            os.system(dir)
-            subprocess.Popen(dir)
-            subprocess.call(dir)
+        elif "schedule" in command:
+            print("you are redirecting to My Class")
+            speak("you are redirecting to My Class")
+            check = myclass()
+            check.schedule()
+
+        elif "exit" in command:
+            speak("Alright! Thankyou for using me.")
+            exit()
+
+        else:
+            speak("Please say it again.")
 
 
+time.sleep(1)
 greet()
-store = record().lower()
-respond(store)
+while True:
+    command = record()
+    respond(command)
